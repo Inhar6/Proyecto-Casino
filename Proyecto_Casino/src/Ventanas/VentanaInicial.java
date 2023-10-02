704aa722b96a9eb414ce99093dbf7ab35825d588
Mario Armellini <mario.armellini@opendeusto.es>
2023-10-02 08:51:03 +0200

    Añadidos botones "Juegos", "Registro" y "Login" en VentanaInicial

--- Proyecto_Casino/src/Ventanas/VentanaInicial.java
+++ Proyecto_Casino/src/Ventanas/VentanaInicial.java
@@ -1,5 +1,34 @@
 package Ventanas;
 
-public class VentanaInicial {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+public class VentanaInicial extends JFrame{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public VentanaInicial() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(1920, 1080);
+		setTitle("Casino");
+		setVisible(true);
+		
+		JPanel myPanel = new JPanel();
+		add(myPanel);
+		
+		JButton Juegos = new JButton("Juegos");
+		myPanel.add(Juegos);
+		JButton Registro = new JButton("Registro");
+		myPanel.add(Registro);
+		JButton Login = new JButton("Login");
+		myPanel.add(Login);
+	}
+	
+	public static void main(String[] args) {
+		new VentanaInicial();
+	}
 }
