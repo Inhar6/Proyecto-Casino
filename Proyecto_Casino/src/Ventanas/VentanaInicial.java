876926054a2169b9610e94d11824ee6820a53ec4
Mario Armellini <mario.armellini@opendeusto.es>
2023-10-09 09:41:23 +0200

    Añadido boton "Login" y creada VentanaLogin

--- Proyecto_Casino/src/Ventanas/VentanaInicial.java
+++ Proyecto_Casino/src/Ventanas/VentanaInicial.java
@@ -1,8 +1,13 @@
 package Ventanas;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
 
 public class VentanaInicial extends JFrame{
 	
@@ -11,21 +16,31 @@ public class VentanaInicial extends JFrame{
 	 */
 	private static final long serialVersionUID = 1L;
 
+	
+	private JButton bLogin = new JButton ("Login");
+	
+	
+	private JPanel pMenu = new JPanel (new BorderLayout());
+	
+	
+	
 	public VentanaInicial() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(1920, 1080);
+		setSize(800, 600);
 		setTitle("Casino");
 		setVisible(true);
 		
-		JPanel myPanel = new JPanel();
-		add(myPanel);
 		
-		JButton Juegos = new JButton("Juegos");
-		myPanel.add(Juegos);
-		JButton Registro = new JButton("Registro");
-		myPanel.add(Registro);
-		JButton Login = new JButton("Login");
-		myPanel.add(Login);
+		pMenu.add(bLogin, BorderLayout.NORTH);
+		add(pMenu);
+		
+		bLogin.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				VentanaLogin vl = new VentanaLogin();
+			}
+		});
 	}
 	
 	public static void main(String[] args) {
