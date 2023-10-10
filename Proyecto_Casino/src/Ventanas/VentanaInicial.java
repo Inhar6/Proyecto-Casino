554ed4324c20f68e8cba2901be4dad4b304b24d4
Mario Armellini <mario.armellini@opendeusto.es>
2023-10-10 16:54:15 +0200

    Añadidos y colocados botones/comboBox en ventana inicial, creadas ventanas BlackJack, Crash, Ruleta y CoinFlip

--- Proyecto_Casino/src/Ventanas/VentanaInicial.java
+++ Proyecto_Casino/src/Ventanas/VentanaInicial.java
@@ -1,12 +1,18 @@
 package Ventanas;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.border.EmptyBorder;
 
 
 public class VentanaInicial extends JFrame{
@@ -18,10 +24,21 @@ public class VentanaInicial extends JFrame{
 
 	
 	private JButton bLogin = new JButton ("Login");
+	private JButton bSingUp = new JButton ("SingUp");
+	private String[] juegos = {"","Black Jack", "Crash", "Ruleta", "Coin-Flip"};
+	private JComboBox<String> comboJuegos = new JComboBox<>(juegos);
 	
+	private ImageIcon logoCasino = new ImageIcon("foto/iconos/logo-no-background.png");
+	private ImageIcon logoCasinoPequeño = new ImageIcon("foto/iconos/logoCasinoPequeño.png");
+
+	private JButton bCasino = new JButton ();
 	
 	private JPanel pMenu = new JPanel (new BorderLayout());
-	
+	private JPanel pMenuN = new JPanel (new BorderLayout());
+	private JPanel pMenuN2 = new JPanel (new BorderLayout());
+	private JPanel pMenuN3 = new JPanel (new BorderLayout());
+	private JPanel pMenuC = new JPanel (new BorderLayout());
+	private JLabel label = new JLabel();
 	
 	
 	public VentanaInicial() {
@@ -29,11 +46,38 @@ public class VentanaInicial extends JFrame{
 		setSize(800, 600);
 		setTitle("Casino");
 		setVisible(true);
-		
-		
-		pMenu.add(bLogin, BorderLayout.NORTH);
+
 		add(pMenu);
 		
+		label.setIcon(redimensionarIcono(logoCasino, 300, 300));
+//		bCasino.setIcon(logoCasinoPequeño);
+//		bCasino.setBorder(null);
+		
+		pMenuC.setBorder(new EmptyBorder(0,250,100,0));
+		pMenu.add(pMenuC,BorderLayout.CENTER);
+		pMenuC.add(label, BorderLayout.SOUTH);
+		
+		pMenu.add(pMenuN, BorderLayout.NORTH);
+		
+		pMenuN.add(bLogin, BorderLayout.EAST);
+		pMenuN.add(pMenuN2, BorderLayout.CENTER);
+		pMenuN.add(bCasino, BorderLayout.WEST);
+        //
+		pMenuN.setBorder(new EmptyBorder(20, 25, 15, 25));
+		pMenuN.setBackground(Color.LIGHT_GRAY);
+
+		pMenuN2.setBorder(new EmptyBorder(0, 5, 0, 5));
+		pMenuN2.setBackground(Color.LIGHT_GRAY);
+		
+		pMenuN3.setBorder(new EmptyBorder(0, 5, 0, 5));
+		pMenuN3.setBackground(Color.LIGHT_GRAY);
+		//
+		pMenuN2.add(new JLabel("Juegos:"),BorderLayout.WEST);
+		pMenuN2.add(bSingUp, BorderLayout.EAST);
+		
+		pMenuN2.add(pMenuN3,BorderLayout.CENTER);
+		pMenuN3.add(comboJuegos,BorderLayout.WEST);
+
 		bLogin.addActionListener(new ActionListener() {
 
 			@Override
@@ -42,8 +86,22 @@ public class VentanaInicial extends JFrame{
 			}
 		});
 	}
+	public ImageIcon redimensionarIcono(ImageIcon imageIcon, int width, int height) {
+		Image image = imageIcon.getImage();
+		Image newimg = image.getScaledInstance(width, height,  java.awt.Image.SCALE_SMOOTH); // escalar la imagen
+		
+		imageIcon = new ImageIcon(newimg);
+		
+		return(imageIcon);
+	}
 	
 	public static void main(String[] args) {
-		new VentanaInicial();
+        SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				new VentanaInicial();				
+			}
+        });
 	}
 }
--- Proyecto_Casino/src/Ventanas/VentanaLogin.java
+++ Proyecto_Casino/src/Ventanas/VentanaLogin.java
@@ -1,5 +1,17 @@
 package Ventanas;
 
-public class VentanaLogin {
+import javax.swing.JFrame;
 
+public class VentanaLogin extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public VentanaLogin() {
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setSize(800, 600);
+	}
+	
 }
