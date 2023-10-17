34b3516397355f197b7a726018ae84f8dad651a4
Inhar Serrano <inhar.serrano@opendeusto.es>
2023-10-17 09:25:09 +0200

    Añadidos ActionListener a los botones

--- Proyecto_Casino/src/Ventanas/VentanaRuleta.java
+++ Proyecto_Casino/src/Ventanas/VentanaRuleta.java
@@ -7,10 +7,9 @@ import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
-import java.util.random.RandomGenerator;
 
 import javax.swing.DefaultListModel;
 import javax.swing.ImageIcon;
@@ -32,7 +31,20 @@ public class VentanaRuleta extends JFrame{
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	
 	private int[] rojo = {1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36};
+	/*
+	//Docenas
+	private int [] docenaP = {1,2,3,4,5,6,7,8,9,10,11,12};
+	private int [] docenaS= {13,14,15,16,17,18,19,20,21,22,23,24};
+	private int [] docenaT= {24,26,27,28,29,30,31,32,33,34,35,36};
+	//Filas
+	private int [] filaP = {};
+	private int[] filaS = {};
+	private int[] filaT = {};
+	*/
+	private List<Integer> apuesta = new ArrayList<Integer>();
+	
 	
 	//Elementos
 	//Botones
@@ -177,14 +189,115 @@ public class VentanaRuleta extends JFrame{
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-					//Imprime en la lista valores del 0 al 10
+				/*	
+				//Imprime en la lista valores del 0 al 10
 					for(int i = 0;i<=10;i++) {
 						dlmHistorial.addElement(i);
 					}
-				
+				*/
+					btnVerde.setEnabled(false);
+					apuesta.add(0);
 				
 			}
 		});
+		btnRojo.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				btnRojo.setEnabled(false);
+				
+			}
+		});
+		btnNegro.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				btnNegro.setEnabled(false);
+				
+			}
+		});
+		btnPar.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				btnPar.setEnabled(false);
+				
+			}
+		});
+		btnImpar.addActionListener(new ActionListener() {
+		
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				btnImpar.setEnabled(false);
+			
+			}
+		});
+		btn1a18.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				btn1a18.setEnabled(false);
+			
+			}
+		});
+		btn19a36.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				btn19a36.setEnabled(false);
+			
+			}
+		});
+		btn1Docena.addActionListener(new ActionListener() {
+					
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				btn1Docena.setEnabled(false);
+			
+			}
+		});
+		btn2Docena.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				btn2Docena.setEnabled(false);
+			
+			}
+		});
+		btn3Docena.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				btn3Docena.setEnabled(false);
+			
+			}
+		});
+
+		btn1Fila.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				btn1Fila.setEnabled(false);
+			
+			}
+		});
+		btn2Fila.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				btn2Fila.setEnabled(false);
+			
+			}
+		});
+		btn3Fila.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				btn3Fila.setEnabled(false);
+			
+			}
+		});
+		
 		
 		btnJugar.addActionListener(new ActionListener() {
 			
@@ -192,6 +305,9 @@ public class VentanaRuleta extends JFrame{
 			public void actionPerformed(ActionEvent e) {
 				int num = random.nextInt(36);
 				dlmHistorial.addElement(num);
+				//double resultado=juego(num, apuesta);
+				//System.out.println(resultado);
+				activarBotones();
 				
 			}
 		});
@@ -235,4 +351,39 @@ public class VentanaRuleta extends JFrame{
 		return false;
 	}
 
+	public void activarBotones() {
+		btnVerde.setEnabled(true);
+		btnRojo.setEnabled(true);
+		btnNegro.setEnabled(true);
+		btnVerde.setEnabled(true);
+			btnVerde.setBackground(Color.green);
+			btnVerde.setForeground(Color.white);
+		btnRojo.setEnabled(true);
+			btnRojo.setBackground(Color.red);
+			btnRojo.setForeground(Color.white);
+		btnNegro.setEnabled(true);
+			btnNegro.setBackground(Color.black);
+			btnNegro.setForeground(Color.white);
+		btn1Docena.setEnabled(true);
+		btn2Docena.setEnabled(true);
+		btn3Docena.setEnabled(true);
+		btn1a18.setEnabled(true);
+		btn19a36.setEnabled(true);
+		btnPar.setEnabled(true);
+		btnImpar.setEnabled(true);
+		btn1Fila.setEnabled(true);
+		btn2Fila.setEnabled(true);
+		btn3Fila.setEnabled(true);
+	}
+	
+	
+	public double juego(int num, List<Integer> apuesta) {
+		double resultado =0;
+		if(apuesta.contains(num)) {
+			
+		}else {
+			resultado=0;
+		}
+		return resultado;
+	}
 }
