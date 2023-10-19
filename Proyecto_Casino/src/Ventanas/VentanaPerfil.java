5487d2454350d48ca8d6525fa821e024359a9fcb
Inhar Serrano <inhar.serrano@opendeusto.es>
2023-10-19 12:00:09 +0200

    Añadidas nuevas funcionalidades en Ventana Perfil

--- Proyecto_Casino/src/Ventanas/VentanaPerfil.java
+++ Proyecto_Casino/src/Ventanas/VentanaPerfil.java
@@ -1,16 +1,24 @@
 package Ventanas;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
 
 public class VentanaPerfil extends JFrame{
 
@@ -19,50 +27,68 @@ public class VentanaPerfil extends JFrame{
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	private String[] listaJuegos = {"Ruleta", "Crash", "BlackJack", "Coin Flip"};
 	//Elementos
 	//JLabels
 	private JLabel nombre;
 	private JLabel nUsuario;
 	private JLabel fechaNacimiento;
 	private JLabel perfil;
+	private JLabel bal;
 	//JTextFields
 	private JTextField txtNombre;
 	private JTextField txtNUsuario;
 	private JTextField txtFechaNacimiento;
-	
+	//JComboBox
+	private JComboBox<String> jcbJuegos;
+	//JTable
+	private JTable tabla;
+	private DefaultTableModel dtmTabla;
+	private JScrollPane scroll;
+	//JButton
+	private JButton edit;
 	
 	public VentanaPerfil() {
 		setIconImage(new ImageIcon("foto/iconos/favicon.png").getImage());
 		setTitle("Perfil del Usuario");
 		setSize(500,400);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setLocationRelativeTo(null);
-		
-		
+		//JLabel
 		nombre = new JLabel("Nombre");
 		nUsuario = new JLabel("Usuario");
 		fechaNacimiento = new JLabel("Fecha de nacimiento");
 		perfil = new JLabel("Perfil");
-		
+		bal = new JLabel("Balance");
+		//JTextField
 		txtNombre= new JTextField(15);
 		txtNUsuario= new JTextField(15);
 		txtFechaNacimiento= new JTextField(15);
+		//JComboBox
+		jcbJuegos= new JComboBox<String>(listaJuegos);
+		//JTable
+		dtmTabla= new DefaultTableModel();
+		tabla = new JTable(dtmTabla);
+		tabla.setEnabled(false);
+		tabla.setDefaultRenderer(Object.class, new MyRender());
+		scroll = new JScrollPane(tabla);
+		//JButton
+		edit= new JButton("Editar");
 		
+		//Paneles
 		JPanel principal = new JPanel();
 		JPanel perf = new JPanel();
 		JPanel central = new JPanel();
-		JPanel derecho = new JPanel();
+		JPanel izquierdo = new JPanel();
 			JPanel N = new JPanel();
 			JPanel NU = new JPanel();
 			JPanel F = new JPanel();
-		JPanel izquierdo = new JPanel();
-		
-		/*  Para añadir el menu superior si se quiere
-		JPanel menuSuperior = new JPanel(new GridLayout());
-		JMenuBar menuBar1 = new JMenuBar();
-		JMenu menu = new JMenu();
-		VentanaPanelMenu x = new VentanaPanelMenu();
-		*/
+		JPanel derecho = new JPanel();
+			JPanel juegos = new JPanel();
+				JPanel box = new JPanel();
+			JPanel balance = new JPanel ();
+				JPanel b = new JPanel();
+		JPanel editar = new JPanel();
 		
 		N.setLayout(new FlowLayout());
 		N.add(nombre);
@@ -73,34 +99,117 @@ public class VentanaPerfil extends JFrame{
 		F.setLayout(new FlowLayout());
 		F.add(fechaNacimiento);
 		F.add(txtFechaNacimiento);
-		derecho.setLayout(new GridLayout(3,1));
-		derecho.add(N);
-		derecho.add(NU);
-		derecho.add(F);
-		izquierdo.setLayout(new GridLayout(2,1));
-		///////
-		//Añadir Historiales
-		///////
+		izquierdo.setLayout(new GridLayout(4,1));
+		izquierdo.add(N);
+		izquierdo.add(NU);
+		izquierdo.add(F);
+		juegos.setLayout(new BorderLayout());
+			box.setLayout(new FlowLayout());
+			box.add(jcbJuegos);
+		juegos.add(box, BorderLayout.NORTH);
+		juegos.add(scroll, BorderLayout.CENTER);
+		///
+		//GRAFICO
+		///
+		balance.setLayout(new BorderLayout());
+			b.setLayout(new FlowLayout());
+			b.add(bal);
+		balance.add(b, BorderLayout.NORTH);
+		///
+		//GRAFICO
+		///
+		derecho.setLayout(new GridLayout(2,1));
+		derecho.add(juegos);
+		derecho.add(balance);
 		central.setLayout(new GridLayout(1,2));
-		central.add(derecho);
 		central.add(izquierdo);
+		central.add(derecho);
 		perf.setLayout(new FlowLayout());
 		perf.add(perfil);
-		
+		editar.add(edit);
 		principal.setLayout(new BorderLayout());
 		principal.add(perf, BorderLayout.NORTH);
 		principal.add(central,BorderLayout.CENTER);
+		principal.add(editar, BorderLayout.SOUTH);
 		
-		//setLayout(new GridLayout(2,1));
-        //add(menuSuperior);
-        add(principal);
-        /*
-        ///
-        setJMenuBar(menuBar1);
-        x.enseñarMenu(menuSuperior, menu);
-		///
-        */
+		add(principal);
+		
+		jcbJuegos.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String juego = (String)jcbJuegos.getSelectedItem();
+				if(juego.equals("Ruleta")) {
+					limpiarTabla();
+					pintarDatosRuleta();
+				}else if(juego.equals("Coin Flip")) {
+					limpiarTabla();
+					pintarDatosCoinFlip();
+				}else if(juego.equals("Crash")) {
+					limpiarTabla();
+					pintarDatosCrash();
+				}else {
+					limpiarTabla();
+					pintarDatosBlackJack();
+				}
+			}
+		});
+
+     
 		setVisible(true);
+		//pack();
+	}
+	public void limpiarTabla() {
+		dtmTabla.setRowCount(0);
+		dtmTabla.setColumnCount(0);
 	}
 	
+	public void pintarDatosRuleta() {
+		dtmTabla.addColumn("Tirada");
+		dtmTabla.addColumn("Resultado");
+		dtmTabla.addColumn("Ganancia");
+		dtmTabla.addRow(new Object[] {1,5,-10});
+		dtmTabla.addRow(new Object[] {2,30,-30});
+		dtmTabla.addRow(new Object[] {3,12,+150});
+	}
+	public void pintarDatosCoinFlip() {
+		dtmTabla.addColumn("Tirada");
+		dtmTabla.addColumn("Resultado");
+		dtmTabla.addColumn("Ganancia");
+		dtmTabla.addRow(new Object[] {1,"Cara",+20});
+		dtmTabla.addRow(new Object[] {2,"Cara",-30});
+		dtmTabla.addRow(new Object[] {3,"Cruz",+150});
+	}
+	public void pintarDatosCrash() {
+
+	}
+	public void pintarDatosBlackJack() {
+
+	}
+	
+	class MyRender extends JLabel implements TableCellRenderer{
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+		public MyRender() {
+			setOpaque(true);
+		}
+		
+		@Override
+		public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
+				int row, int column) {
+			
+			setText(value.toString());
+			if(row%2 ==0) {
+				setBackground(Color.cyan);
+			}else {
+				setBackground(Color.white);
+			}
+			
+			return this;
+		}
+		
+	}
 }
