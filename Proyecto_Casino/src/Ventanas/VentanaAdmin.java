8bc67704c74288218aa3834dbb8ac4d7f8d34bf4
Inhar Serrano <inhar.serrano@opendeusto.es>
2023-10-25 09:35:59 +0200

    Añadida VentanaAdmin de prueba

---
+++ Proyecto_Casino/src/Ventanas/VentanaAdmin.java
@@ -0,0 +1,70 @@
+package Ventanas;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+public class VentanaAdmin extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	//Elementos
+	private JLabel admin;
+	
+	private JButton btnJuegos;
+	private JButton btnUsuarios;
+	
+	
+	public VentanaAdmin() {
+		setTitle("Ventana del Administrador");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setSize(800,700);
+		setLocationRelativeTo(null);
+		
+		admin = new JLabel("                          Vista de administrador");
+		btnJuegos= new JButton("Estadisticas de los juegos");
+		btnUsuarios= new JButton("Datos de los usuarios");
+		
+		JPanel pPrincipal = new JPanel();
+		
+		
+		pPrincipal.setLayout(new FlowLayout());
+		pPrincipal.add(btnJuegos);
+		pPrincipal.add(btnUsuarios);
+		
+		setLayout(new BorderLayout());
+		add(admin, BorderLayout.NORTH);
+		add(pPrincipal,BorderLayout.CENTER);
+		pack();
+		
+		btnUsuarios.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new VentanaAdminUsuarios();
+				
+			}
+		});
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				new VentanaAdmin();
+			}
+		});
+	}
+}
