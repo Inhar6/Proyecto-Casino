339926c62d4a21152011f6978412864748342577
Inhar Serrano <inhar.serrano@opendeusto.es>
2023-11-18 16:38:14 +0100

    Creada clase Fichero con funcion de lectura de ficheros

---
+++ Proyecto_Casino/src/io/Fichero.java
@@ -0,0 +1,28 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public class Fichero {
+	
+	private static final Logger logger = Logger.getLogger("Ventana Administrador Juegos");
+	
+	//Lectura de ficheros
+	public static String leerFichero(String ruta) {
+		 StringBuilder texto = new StringBuilder();
+	        try (BufferedReader br = new BufferedReader(new FileReader(ruta))) {
+	            String linea;
+	            while ((linea = br.readLine()) != null) {
+	            	texto.append(linea).append("\n");
+	            }
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	            logger.info("Fallo en la lectura del fichero");
+	       }
+	        return texto.toString();
+	}
+	
+	
+}
