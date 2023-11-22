2d1cefdd87bc106bd8d122eaabcab0f7bade1c47
Inhar Serrano <inhar.serrano@opendeusto.es>
2023-11-22 13:35:28 +0100

    Añadidos nuevos metodos para la creacion de tablas

--- Proyecto_Casino/src/db/DBManager.java
+++ Proyecto_Casino/src/db/DBManager.java
@@ -2,6 +2,7 @@ package db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -13,7 +14,7 @@ import domain.Usuario;
 public class DBManager {
 	
 	private Connection conn = null;
-	
+	public static final String URL = "resources/db/NoEscasino.db";
 	public void connect(String dbPath) {
 		try {
 			Class.forName("org.sqlite.JDBC");
@@ -55,9 +56,82 @@ public class DBManager {
 		}
 		return lstUsuarios;
 	}
+	
+	public void crearTablaUsuario() {
+		try(Statement stmt = conn.createStatement()) {
+			stmt.executeUpdate("CREATE TABLE IF NOT EXISTS Usuario (\n"
+					+ "    	nombre VARCHAR(255),\n"
+					+ "    	apellidos VARCHAR(255),\n"
+					+ "    	dni VARCHAR(20),\n"
+					+ "    	saldo DOUBLE,\n"
+					+ "    	numero_cuenta INT,\n"
+					+ "    	contrasena VARCHAR(255),\n"
+					+ "    	nombre_usuario VARCHAR(50) PRIMARY KEY ); ");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void añadirUsuariosEjemplo() {
+		try (PreparedStatement pstmt = conn.prepareStatement("INSERT INTO Usuario (nombre, apellidos, dni, saldo, numero_cuenta, contrasena, nombre_usuario) VALUES\r\n"
+				+ "    		('Nombre1', 'Apellido1', '11111111A', 1000.0, 12345, 'contrasena1', 'usuario1'),\r\n"
+				+ "    		('Nombre2', 'Apellido2', '22222222B', 1500.0, 67890, 'contrasena2', 'usuario2'),\r\n"
+				+ "			('Nombre3', 'Apellido3', '33333333C', 75000.0, 54321, 'contrasena3', 'usuario3'),\r\n"
+				+ "			('Nombre4', 'Apellido4', '44444444D', 20000.0, 98765, 'contrasena4', 'usuario4'),\r\n"
+				+ "			('Nombre5', 'Apellido5', '55555555E', 300.0, 24680, 'contrasena5', 'usuario5'),\r\n"
+				+ "			('Nombre6', 'Apellido6', '66666666F', 450000.0, 11223, 'contrasena6', 'usuario6'),\r\n"
+				+ "			('Nombre7', 'Apellido7', '77777777G', 55000.0, 33221, 'contrasena7', 'usuario7'),\r\n"
+				+ "			('Nombre8', 'Apellido8', '88888888H', 1200.0, 76543, 'contrasena8', 'usuario8'),\r\n"
+				+ "			('Nombre9', 'Apellido9', '99999999I', 800.0, 19876, 'contrasena9', 'usuario9'),\r\n"
+				+ "			('Nombre10', 'Apellido10', '10101010J', 2200.0, 23456, 'contrasena10', 'usuario10'),\r\n"
+				+ "			('Nombre11', 'Apellido11', '11111111K', 170000.0, 90876, 'contrasena11', 'usuario11'),\r\n"
+				+ "			('Nombre12', 'Apellido12', '12121212L', 1600.0, 65432, 'contrasena12', 'usuario12'),\r\n"
+				+ "			('Nombre13', 'Apellido13', '13131313M', 1800.0, 56789, 'contrasena13', 'usuario13'),\r\n"
+				+ "			('Nombre14', 'Apellido14', '14141414N', 4000.0, 11234, 'contrasena14', 'usuario14'),\r\n"
+				+ "			('Nombre15', 'Apellido15', '15151515O', 300000.0, 98765, 'contrasena15', 'usuario15');	")){
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void crearTablaRuleta() {
+		try (Statement stmt = conn.createStatement()){
+			stmt.executeUpdate(" CREATE TABLE IF NOT EXISTS Ruleta (\n"
+					+ "    	numero INTEGER,\n"
+					+ "    	ganancia DOUBLE,\n"
+					+ "    	tirada INTEGER,\n"
+					+ "    	nombre_usuario VARCHAR(50),\n"
+					+ "    FOREIGN KEY (nombre_usuario) REFERENCES Usuario(nombre_usuario));");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	public void añadirRuletaEjemplo() {
+		try (PreparedStatement pstmt = conn.prepareStatement("INSERT INTO Ruleta (numero, ganancia, tirada, nombre_usuario) VALUES\r\n"
+				+ "	    (1, 100.0, 10, 'usuario1'),\r\n"
+				+ "	    (2, 150.0, 15, 'usuario2'),\r\n"
+				+ "	    (3, 80.0, 8, 'usuario3'),\r\n"
+				+ "	    (4, 200.0, 20, 'usuario4'),\r\n"
+				+ "	    (5, 120.0, 12, 'usuario5'),\r\n"
+				+ "	    (6, 90.0, 9, 'usuario6'),\r\n"
+				+ "	    (7, 180.0, 18, 'usuario7'),\r\n"
+				+ "	    (8, 60.0, 6, 'usuario8'),\r\n"
+				+ "	    (9, 130.0, 13, 'usuario9'),\r\n"
+				+ "	    (10, 110.0, 11, 'usuario10'),\r\n"
+				+ "	    (11, 170.0, 17, 'usuario11'),\r\n"
+				+ "	    (12, 50.0, 5, 'usuario12'),\r\n"
+				+ "	    (13, 140.0, 14, 'usuario13'),\r\n"
+				+ "	    (14, 95.0, 9, 'usuario14'),\r\n"
+				+ "	    (15, 160.0, 16, 'usuario15');")){
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 	/*
 	 * TABLA USUARIOS
-	CREATE TABLE IF NOT EXISTS Usuarios (
+	CREATE TABLE IF NOT EXISTS Usuario (
     	nombre VARCHAR(255),
     	apellidos VARCHAR(255),
     	dni VARCHAR(20),
@@ -68,7 +142,7 @@ public class DBManager {
 	); 
 	
 		DATOS PARA INSERTAR EN LA TABLA Usuarios
-		INSERT INTO Usuarios (nombre, apellidos, dni, saldo, numero_cuenta, contrasena, nombre_usuario) VALUES
+		INSERT INTO Usuario (nombre, apellidos, dni, saldo, numero_cuenta, contrasena, nombre_usuario) VALUES
     		('Nombre1', 'Apellido1', '11111111A', 1000.0, 12345, 'contrasena1', 'usuario1'),
     		('Nombre2', 'Apellido2', '22222222B', 1500.0, 67890, 'contrasena2', 'usuario2'),
 			('Nombre3', 'Apellido3', '33333333C', 75000.0, 54321, 'contrasena3', 'usuario3'),
@@ -93,11 +167,11 @@ public class DBManager {
     	ganancia DOUBLE,
     	tirada INTEGER,
     	nombre_usuario VARCHAR(50),
-    FOREIGN KEY (nombre_usuario) REFERENCES Usuarios(nombre_usuario)
+    FOREIGN KEY (nombre_usuario) REFERENCES Usuario(nombre_usuario)
 	);
 	
 	DATOS A INSERTAR EN LA TABLA Ruleta
-	INSERT INTO Juegos (numero, ganancia, tirada, nombre_usuario) VALUES
+	INSERT INTO Ruleta (numero, ganancia, tirada, nombre_usuario) VALUES
 	    (1, 100.0, 10, 'usuario1'),
 	    (2, 150.0, 15, 'usuario2'),
 	    (3, 80.0, 8, 'usuario3'),
