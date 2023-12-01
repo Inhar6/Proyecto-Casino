f7eb43c217946617b39a1554b718caf24a43c589
Inhar Serrano <inhar.serrano@opendeusto.es>
2023-12-01 20:11:49 +0100

    Solucionado error de borrado en la base de datos

--- Proyecto_Casino/src/db/DBCreator.java
+++ Proyecto_Casino/src/db/DBCreator.java
@@ -10,14 +10,10 @@ public class DBCreator {
 		
 		System.out.println("Conectando con la base de datos...");
 		
-		
+		//Creacion de tablas
 		System.out.println("Creando tabla Usuario");
 		DBManager.crearTablaUsuario();
-		/*
-		System.out.println("Añadiendo Usuarios de ejemplo");
-		db.añadirUsuariosEjemplo();
-		*/
-		
+	
 		System.out.println("Creando tabla CuentaBancaria");
 		DBManager.crearTablaCuentaBancaria();
 		
@@ -26,26 +22,31 @@ public class DBCreator {
 		
 		System.out.println("Creando tabla Crash");
 		DBManager.crearTablaCrash();
-		/*
-		System.out.println("Añadiendo Datos de ejemplo en ruleta");
-		db.añadirRuletaEjemplo();
-		*/
-		/*
-		System.out.println("Añadiendo Datos de ejemplo en Crash");
-		DBManager.añadirCrashEjemplo();
-		*/
+		
 		System.out.println("Creando tabla Black Jack");
 		DBManager.crearTablaBlackJack();
+		
+		System.out.println("Creando tabla Balance");
+		DBManager.crearTablaBalance();
+		
 		/*
+		//Agregacion de datos de prueba a las tablas
+		System.out.println("Añadiendo Usuarios de ejemplo");
+		DBManager.añadirUsuariosEjemplo();
+		
+		System.out.println("Añadiendo Datos de ejemplo en ruleta");
+		DBManager.añadirRuletaEjemplo();
+		
+		System.out.println("Añadiendo Datos de ejemplo en Balance");
+		DBManager.añadirBalanceEjemplo();
+		
+		System.out.println("Añadiendo Datos de ejemplo en Crash");
+		DBManager.añadirCrashEjemplo();
+		
 		System.out.println("Añadiendo Datos de ejemplo en Black Jack");
 		DBManager.añadirBlackJackEjemplo();
 		*/
-		System.out.println("Creando tabla Balance");
-		DBManager.crearTablaBalance();
-		/*
-		System.out.println("Añadiendo Datos de ejemplo en Balance");
-		DBManager.añadirBalanceEjemplo();
-		*/
+		
 		System.out.println("Cerrando conexion...");
 	}
 }
